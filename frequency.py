7d78f80bbbc874b4cca9d9186c949cfdfae65846
zyinshi <zyinshi@gmail.com>
2014-07-19 11:35:48 -0700

    format changes

--- frequency.py
+++ frequency.py
@@ -14,38 +14,6 @@ def parseData(l):
 	encoded_tweet = tweet.encode('utf-8')
 	return encoded_tweet
 
-def buildDict(fp):
-	scores = {}
-	for line in fp:
-		term, score = line.split("\t")
-		scores[term] = int(score)
-	return scores
-
-def newDict(fp, dic):
-	undefined_term = {}
-	for line in fp:
-		tweet_str = parseData(line)
-		for word in tweet_str.split(" "):
-			if(dic.has_key(word)==False):
-				undefined_term[word]=[0,0,0]
-	return undefined_term
-
-def computeSent(fp, num_tweet, text_sent, terms):
-	for i in range(num_tweet):
-		tweet_str = parseData(fp[i])
-		for word in tweet_str.split(" "): 
-			if(terms.has_key(word)):
-				terms[word][0] += text_sent[i]
-				terms[word][1] += 1
-				#print "*************", terms[word][0]
-	for value in terms.values():
-		#print value[0]
-		if(value[1]==0):
-			value[2]=0
-		else:
-			alpha = value[0]/value[1]
-			value[2] = 5/(1+math.exp(-alpha))-0.5
-	return terms
 
 
 def computeFreq(fp):
